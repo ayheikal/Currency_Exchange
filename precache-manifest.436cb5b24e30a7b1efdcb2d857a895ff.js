c17b3cf4342658c534723377fd695258d10e8872
ayheikal <heikal1998@gmail.com>
2020-04-30 18:23:33 +0200

    Updates

---
+++ precache-manifest.436cb5b24e30a7b1efdcb2d857a895ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1940159909cfbe580c59abf2a69a8846",
+    "url": "/Currency_Exchange/index.html"
+  },
+  {
+    "revision": "b02b1f500590388d9c75",
+    "url": "/Currency_Exchange/static/css/main.8d16d90f.chunk.css"
+  },
+  {
+    "revision": "861b488f81aea79cedb4",
+    "url": "/Currency_Exchange/static/js/2.5e7aeca7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Currency_Exchange/static/js/2.5e7aeca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02b1f500590388d9c75",
+    "url": "/Currency_Exchange/static/js/main.9eb75cd9.chunk.js"
+  },
+  {
+    "revision": "a49cecf73bdd4013d83b",
+    "url": "/Currency_Exchange/static/js/runtime-main.b8f34609.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Currency_Exchange/static/media/logo.5d5d9eef.svg"
+  }
+]);
